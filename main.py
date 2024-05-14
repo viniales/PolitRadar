3fc1f0fc57014183ab6b0d6a2505767e75ea1528
Viniales12 <viniales12@gmail.com>
2024-05-14 15:40:14 +0200

    add context manager

--- main.py
+++ main.py
@@ -8,21 +8,19 @@ user_agent = os.environ.get('user_agent')
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
 
 # Subreddity do monitorowania
-subreddits = ["Trump", "Biden", "election2024"]
+subreddits = ["Trump", "Biden"]
 
 # Pobieranie postów z Redditu
 posts = []
 for subreddit_name in subreddits:
-    subreddit = reddit.subreddit(subreddit_name)
-    new_posts = subreddit.top(time_filter="day", limit=30)  # Pobierz 10 najpopularniejszych postów z ostatniego dnia
+    new_posts = reddit.subreddit(subreddit_name).top(time_filter="day", limit=10)  # Pobierz 10 najpopularniejszych postów z ostatniego dnia
     for post in new_posts:
         posts.append(post)
 
-count = 0
+
 # Wyświetlenie pobranych postów (do celów testowych)
 for post in posts:
-    count += 1
-    print(" ")
-    print(post.title)
+    with open('tekst.txt','a') as file:
+        file.write(post.title+'\n')
 
-print(count)
+
