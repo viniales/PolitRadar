b6ff34a385e2d0a9f4b8ca58056edbd91eaef334
Viniales12 <viniales12@gmail.com>
2024-05-14 16:00:55 +0200

    build spark DataFrame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import praw
 import os
+from pyspark.sql import SparkSession
 
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
@@ -7,20 +8,15 @@ user_agent = os.environ.get('user_agent')
 
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
 
-# Subreddity do monitorowania
 subreddits = ["Trump", "Biden"]
 
-# Pobieranie postów z Redditu
 posts = []
 for subreddit_name in subreddits:
-    new_posts = reddit.subreddit(subreddit_name).top(time_filter="day", limit=10)  # Pobierz 10 najpopularniejszych postów z ostatniego dnia
+    new_posts = reddit.subreddit(subreddit_name).top(time_filter="day", limit=10)
     for post in new_posts:
         posts.append(post)
 
+spark = SparkSession.builder.appName('PolitRadar').getOrCreate()
 
-# Wyświetlenie pobranych postów (do celów testowych)
-for post in posts:
-    with open('tekst.txt','a') as file:
-        file.write(post.title+'\n')
-
-
+spark_posts = spark.createDataFrame([(post.title, post.selftext) for post in posts], ["title", "text"])
+spark_posts.show()
