6f43bd1aa00966a1eafc1e85b5220337882f1ff1
Viniales12 <viniales12@gmail.com>
2024-05-14 16:34:52 +0200

    + clear text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import praw
 import os
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
@@ -12,11 +13,13 @@ subreddits = ["Trump", "Biden"]
 
 posts = []
 for subreddit_name in subreddits:
-    new_posts = reddit.subreddit(subreddit_name).top(time_filter="day", limit=10)
+    new_posts = reddit.subreddit(subreddit_name).top(time_filter="day", limit=100)
     for post in new_posts:
         posts.append(post)
 
 spark = SparkSession.builder.appName('PolitRadar').getOrCreate()
 
-spark_posts = spark.createDataFrame([(post.title, post.selftext) for post in posts], ["title", "text"])
+spark_posts = spark.createDataFrame([(post.title, post.selftext) for post in posts], ['title', 'text'])
+spark_posts = spark_posts.withColumn('text', concat(col('title'), lit(' '), col('text')))
+spark_posts = spark_posts.withColumn('clear_text', lower(regexp_replace(col('text'),'[^a-zA-Z\\s]', '')))
 spark_posts.show()
