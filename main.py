03657e946c8268f189e62fac02004d036e41e9cb
Viniales12 <viniales12@gmail.com>
2024-05-18 00:27:41 +0200

    adding column with subreddit name

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import praw
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.ml.feature import Tokenizer
-from pyspark.ml.feature import StopWordsRemover
 from nltk.sentiment import SentimentIntensityAnalyzer
 from pyspark.sql.types import FloatType
 
@@ -12,19 +10,19 @@ client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
 user_agent = os.environ.get('user_agent')
 
+spark = SparkSession.builder.appName('PolitRadar').getOrCreate()
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
 
 subreddits = ["Trump", "Biden"]
 
 posts = []
 for subreddit_name in subreddits:
-    new_posts = reddit.subreddit(subreddit_name).top(time_filter="day", limit=100)
+    new_posts = reddit.subreddit(subreddit_name).top(time_filter="day", limit=3)
     for post in new_posts:
-        posts.append(post)
+        posts.append((subreddit_name, post.title, post.selftext))
 
-spark = SparkSession.builder.appName('PolitRadar').getOrCreate()
-
-spark_posts = spark.createDataFrame([(post.title, post.selftext) for post in posts], ['title', 'text'])
+# Tworzenie DataFrame w Spark
+spark_posts = spark.createDataFrame(posts, ["subreddit", "title", "text"])
 spark_posts = spark_posts.withColumn('text', concat(col('title'), lit(' '), col('text')))
 spark_posts = spark_posts.withColumn('clear_text', lower(regexp_replace(col('text'), '[^a-zA-Z\\s]', '')))
 
@@ -36,11 +34,12 @@ def analyze_sentiment(text):
     sentiment = sia.polarity_scores(text)
     return sentiment['compound']
 
+
 sentiment_udf = udf(analyze_sentiment, FloatType())
 spark_posts = spark_posts.withColumn("sentiment", sentiment_udf("clear_text"))
 
 # Wyświetlenie wyników analizy sentymentu
-spark_posts.select("clear_text", "sentiment").show()
+spark_posts.show()
 
 # Zakończenie sesji Sparkclear
 spark.stop()
