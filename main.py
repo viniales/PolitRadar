918a94fdbe53a8b0c0800d525fd1c8d206e9fc58
Viniales12 <viniales12@gmail.com>
2024-05-17 23:41:02 +0200

    adding stop words remover

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.ml.feature import Tokenizer
+from pyspark.ml.feature import StopWordsRemover
 
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
@@ -26,5 +27,8 @@ spark_posts = spark_posts.withColumn('clear_text', lower(regexp_replace(col('tex
 
 tokenizer = Tokenizer(inputCol='clear_text', outputCol='tokens')
 spark_posts = tokenizer.transform(spark_posts)
+
+remover = StopWordsRemover(inputCol="tokens", outputCol="filtered_tokens")
+spark_posts = remover.transform(spark_posts)
 
 spark_posts.show()
